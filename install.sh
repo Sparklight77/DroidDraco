30850aa7b5299fb233670975966725a7c5bbb345
Sparklight77 <143299299+Sparklight77@users.noreply.github.com>
2024-06-06 09:23:14 +0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Update Termux packages
+pkg update -y && pkg upgrade -y
+
+# Install figlet
+pkg install figlet -y
+
+# Install injector
+curl -o draco_injector.sh https://raw.githubusercontent.com/Sparklight77/Auto-Draco-Injector/main/draco_injector.sh
+
+# Remove the script itself
+rm -- "$0"
+
+#message
+echo" Execute "draco_injector.sh" to Start injector"
